aa4f330148a32a753ecdea415cd57d23c76b2328
Andrés <37509643+jamorafo@users.noreply.github.com>
2019-04-23 19:39:59 -0400

    Not needed.

--- src/prueba.R
+++
@@ -1,9 +0,0 @@
-################################################################################
-# #  Misc. Functions
-################################################################################
-# Scaler function
-min_max = function(x,a=0,b=1){
-  X_std = (x- min(x,na.rm=TRUE))/(max(x,na.rm=TRUE)-min(x,na.rm=TRUE))
-  X_scaled = X_std * (b - a) + a
-  return(X_scaled)
-}
