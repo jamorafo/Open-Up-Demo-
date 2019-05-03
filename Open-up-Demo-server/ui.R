136c9fb4e3679e36b2bd0446e4f2aec4553a14fe
Andrés <jamorafo@gmail.com>
2019-05-02 20:26:12 -0400

    shinyWidgets autoinstaller

--- Open-up-Demo-server/ui.R
+++ Open-up-Demo-server/ui.R
@@ -8,7 +8,7 @@
 # 
 #    http://shiny.rstudio.com/
 #
-list.of.packages <- c("shiny","shinythemes","devtools","shinyjs")
+list.of.packages <- c("shiny","shinythemes","devtools","shinyjs","shinyWidgets")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 
